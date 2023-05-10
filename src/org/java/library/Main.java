20e7a861de29613b3f9678eac3d7019fc4b5553b
Alessandro-Villani <alessvill@libero.it>
2023-05-10 15:03:24 +0200

    added saveBooks and printBooks

--- src/org/java/library/Main.java
+++ src/org/java/library/Main.java
@@ -1,5 +1,9 @@
 package org.java.library;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -23,9 +27,20 @@ public class Main {
 				System.out.println("Inserisci Libro " + (i+1) + " di " + books.length);
 				System.out.println("Inserisci titolo");
 				String title = sc.nextLine();
-				System.out.println("Inserisci numero pagine");
-				int pagesNumber = sc.nextInt();
-				sc.nextLine();
+				int pagesNumber = 0;
+				boolean hasPages = false;
+				while(hasPages == false) {
+					System.out.println("Inserisci numero pagine");
+					try {
+						pagesNumber = sc.nextInt();
+						sc.nextLine();
+						hasPages = true;
+					} catch (Exception e) {
+						System.out.println("Numero di pagine non valido");
+						sc.nextLine();
+					}
+					
+				}
 				System.out.println("Inserisci autore");
 				String author = sc.nextLine();
 				System.out.println("Inserisci editore");
@@ -45,7 +60,54 @@ public class Main {
 			
 		}
 		
+		sc.close();
+		
+		saveBooks(books);
+		
+		printBooks();
+		
 
+	}
+	
+	public static void saveBooks(Book[] books) {
+		
+		String content = "";
+		
+		for (int i = 0; i<books.length; i++) {
+			
+			content += books[i] + "\n";
+			
+		}
+		
+		try {
+			FileWriter wr = new FileWriter("C:\\java_projects\\books.txt");
+			wr.write(content);
+			wr.close();
+			
+		} catch (IOException e) {
+			System.out.println("Errore nella scrittura del file " + e.getMessage());
+		}
+		
+	}
+	
+	public static void printBooks() {
+		
+		File books = new File("C:\\java_projects\\books.txt");
+		try {
+			Scanner reader = new Scanner(books);
+			System.out.println("\n----------------------\nELENCO LIBRI\n----------------------\n");
+			while(reader.hasNextLine()) {
+				System.out.println(reader.nextLine());
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("Errore nella lettura del file" + e.getMessage());
+			
+		}
+		
+		
+		
 	}
 
 }
